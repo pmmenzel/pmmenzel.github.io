4dd1b257390fd9db5db76abcfd63bc720de84b86
Patrick M. Menzel <pmmenzel@gmail.com>
2014-11-13 15:18:58 -0800

    Add Javascript Files - Add Bootstrap.js - Add Parallax.js

---
+++ js/parallax.js
@@ -0,0 +1,23 @@
+//Parallax effect
+
+$objWindow = $(window);
+$('div[data-type="background"]').each(function(){
+  var $bgObj = $(this);
+  $(window).scroll(function() {
+    var yPos = -($objWindow.scrollTop() / $bgObj.data('speed'));
+
+    var coords = '100% ' + yPos + 'px';
+    // Animate background
+    $bgObj.css({ backgroundPosition: coords });
+  });
+});
+
+// Animates scroll waypoints
+
+$(".fa-chevron-down").on("click", function(e) {
+    // Find the next section
+    var nextSection = $(this).parent().parent().next();
+    // Bring next section to the top of the view port, slow scroll with animate
+    $('body').animate({scrollTop:nextSection.offset().top},500);
+    return false;
+});
